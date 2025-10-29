36c268e3536fafebaed4efaa9a2681bf0b633f52
Yosshiii43 <daisy440440@gmail.com>
2025-10-29 19:49:23 +0900

    change:足跡の表示方法

--- js/main.js
+++ js/main.js
@@ -21,3 +21,28 @@ document.addEventListener('DOMContentLoaded', () => {
   const io = new IntersectionObserver(callback); //初期化
   io.observe(hero); //監視を開始
 });
+
+//--------------------------------------------------------------------------
+// 足跡をじわっと出す
+//--------------------------------------------------------------------------
+
+document.addEventListener('DOMContentLoaded', () => {
+ const targets = document.querySelectorAll(".c-blurTrigger");
+
+  const options = {
+    root: null, // ビューポートを基準
+    rootMargin: "0px 0px -60px 0px", // 下方向に60px分早めに発火
+    threshold: 0.5, // 全部入ったら検知
+  };
+
+  const observer = new IntersectionObserver((entries, obs) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("is-blurActive");
+        obs.unobserve(entry.target);// 一度反応したら監視を停止
+      }
+    });
+  }, options);
+
+  targets.forEach((target) => observer.observe(target));
+  });
