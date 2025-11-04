9c7462c54319bb783ea9895f897e0d2b05790f78
Yosshiii43 <daisy440440@gmail.com>
2025-11-04 09:06:21 +0900

    change:delayを0.6から0.8に

--- js/main.js
+++ js/main.js
@@ -165,7 +165,7 @@ document.addEventListener('DOMContentLoaded', () => {
   new simpleParallax(parallaxImages, {
     orientation: 'up', // 上方向に動く（他に 'down', 'left', 'right' なども指定可能）
     scale: 1.4,        // スクロールに合わせて画像を拡大しながら動かす（背景が見切れないように）
-    delay: 0.6,        // 動きに遅延をつけて滑らかに
+    delay: 0.8,        // 動きに遅延をつけて滑らかに
     transition: 'cubic-bezier(0,0,0,1)', // イージングを調整
     overflow: true     // 親要素を基準にして動かす（画面外にあるときのちらつきを防ぐ）
   });
