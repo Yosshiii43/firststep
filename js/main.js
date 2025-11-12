5cffa63dbbf56239a18872d24f46a48b6fae5381
Yosshiii43 <daisy440440@gmail.com>
2025-11-12 17:47:29 +0900

    remove:足跡ふわっと表示

--- js/main.js
+++ js/main.js
@@ -23,31 +23,6 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 //--------------------------------------------------------------------------
-// 足跡をじわっと出す
-//--------------------------------------------------------------------------
-/*
-document.addEventListener('DOMContentLoaded', () => {
- const targets = document.querySelectorAll(".c-blurTrigger");
-
-  const options = {
-    root: null, // ビューポートを基準
-    rootMargin: "0px 0px -100px 0px", // 下方向に100px分早めに発火
-    threshold: 0.3, // 30%入ったら検知
-  };
-
-  const observer = new IntersectionObserver((entries, obs) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add("is-blurActive");
-        obs.unobserve(entry.target);// 一度反応したら監視を停止
-      }
-    });
-  }, options);
-
-  targets.forEach((target) => observer.observe(target));
-  });
-*/
-//--------------------------------------------------------------------------
 // お問い合わせフォーム
 //--------------------------------------------------------------------------
 document.querySelectorAll('.js-form').forEach(form => {
